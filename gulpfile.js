de82674b6d18909f440dff87d09145ac8e844ddb
YuriDonato <yuridonatosis@gmail.com>
2023-09-08 18:25:31 -0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const imagemin = require("gulp-imagemin");
-const ugilify = require("gulp-uglify");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function scripts() {
-    return gulp
-        .src("./src/scripts/*.js")
-        .pipe(ugilify())
-        .pipe(gulp.dest("./dist/js"));
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
-    return gulp
-        .src("./src/styles/*.scss")
-        .pipe(sass({ outputStyle: "compressed" }))
-        .pipe(gulp.dest("./dist/css"));
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-    return gulp
-        .src("./src/images/**/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("./dist/images"));
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
-exports.watch = function () {
-    gulp.watch("./src/styles/*scss", gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-};
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+}
